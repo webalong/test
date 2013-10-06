cc09bfa536d0dcb6924d8aa26c3d2b7c107d854f
webalong <webalong@gmail.com>
2013-10-06 15:23:08 -0500

    first commit

---
+++ interview.rb
@@ -0,0 +1,29 @@
+foo = 5
+bar = 12
+
+puts foo
+puts bar
+
+a = foo and bar
+
+puts a
+
+a = (foo and bar)
+
+puts a
+
+b = (bar and foo)
+
+puts "B = #{b}"
+
+a = (foo && bar)
+puts a
+
+a = bar && foo
+puts a
+
+a = foo & bar
+
+puts a
+
+
